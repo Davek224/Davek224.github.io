06038e25d0fcb0c356d5379887b791d411a8b8c9
Davek224 <31508680+Davek224@users.noreply.github.com>
2022-12-14 18:17:01 +0200

    Update initPendo.js

--- js/initPendo.js
+++ js/initPendo.js
@@ -29,27 +29,27 @@
 
 // in your authentication promise handler or callback
 // in your authentication promise handler or callback
-pendo.initialize({
-    visitor: {
-        id:              'testResponse35'   // Required if user is logged in
-        // email:        // Recommended if using Pendo Feedback, or NPS Email
-        // full_name:    // Recommended if using Pendo Feedback
-        // role:         // Optional
+// pendo.initialize({
+//     visitor: {
+//         id:              'testResponse35'   // Required if user is logged in
+//         // email:        // Recommended if using Pendo Feedback, or NPS Email
+//         // full_name:    // Recommended if using Pendo Feedback
+//         // role:         // Optional
 
-        // You can add any additional visitor level key-values here,
-        // as long as it's not one of the above reserved names.
-    },
+//         // You can add any additional visitor level key-values here,
+//         // as long as it's not one of the above reserved names.
+//     },
 
-    account: {
-        id:           'ACCOUNT-UNIQUE-ID2212Dave2' // Required if using Pendo Feedback
-        // name:         // Optional
-        // is_paying:    // Recommended if using Pendo Feedback
-        // monthly_value:// Recommended if using Pendo Feedback
-        // planLevel:    // Optional
-        // planPrice:    // Optional
-        // creationDate: // Optional
+//     account: {
+//         id:           'ACCOUNT-UNIQUE-ID2212Dave2' // Required if using Pendo Feedback
+//         // name:         // Optional
+//         // is_paying:    // Recommended if using Pendo Feedback
+//         // monthly_value:// Recommended if using Pendo Feedback
+//         // planLevel:    // Optional
+//         // planPrice:    // Optional
+//         // creationDate: // Optional
 
-        // You can add any additional account level key-values here,
-        // as long as it's not one of the above reserved names.
-    }
-});
+//         // You can add any additional account level key-values here,
+//         // as long as it's not one of the above reserved names.
+//     }
+// });
