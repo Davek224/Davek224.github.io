aae9b19168137807b908b1b2abb3daf44f836494
Davek224 <31508680+Davek224@users.noreply.github.com>
2022-12-14 18:08:14 +0200

    Update initPendo.js

--- js/initPendo.js
+++ js/initPendo.js
@@ -1,15 +1,24 @@
 
 
 
-let apiKey = new URLSearchParams(window.location.search).get("apikey");
-apiKey = apiKey || 'someDefaultApiKey';
+// let apiKey = new URLSearchParams(window.location.search).get("apikey");
+// apiKey = apiKey || 'someDefaultApiKey';
+// (function(apiKey){
+//     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+//         v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+//             o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+//         y=e.createElement(n);y.async=!0;y.src='https://pendo-dev-static.storage.googleapis.com/agent/static/'+apiKey+'/pendo.js';
+//         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+// })('08d6bcaf-4fa5-4d41-5b14-428db3c622c6');
+
 (function(apiKey){
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
         v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
             o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-        y=e.createElement(n);y.async=!0;y.src='https://pendo-dev-static.storage.googleapis.com/agent/static/'+apiKey+'/pendo.js';
+        y=e.createElement(n);y.async=!0;y.src='https://staging-cdn.pendo-dev.com/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-})('08d6bcaf-4fa5-4d41-5b14-428db3c622c6');
+})('8d8c765b-239b-4fc9-5418-921143a0c448');
+
 
 
 
