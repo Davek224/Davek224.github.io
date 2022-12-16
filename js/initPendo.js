d00714376fb2ac2daf41b796d70a9585f8637226
Davek224 <31508680+Davek224@users.noreply.github.com>
2022-12-16 12:19:22 +0200

    Update initPendo.js

--- js/initPendo.js
+++ js/initPendo.js
@@ -25,27 +25,27 @@ let appKey = new URLSearchParams(window.location.search).get("apikey");
     // This function uses the placeholder 'ACCOUNT-UNIQUE-ID' value for account ID unless you change the account id field to use your app's values
     // Call this function in your authentication promise handler or callback when your visitor and account id values are available
     // Please use Strings, Numbers, or Bools for value types.
-    // pendo.initialize({
-    //     visitor: {
-    //         id:              'VISITOR-UNIQUE-ID2'   // Required if user is logged in, default creates anonymous ID
-    //         // email:        // Recommended if using Pendo Feedback, or NPS Email
-    //         // full_name:    // Recommended if using Pendo Feedback
-    //         // role:         // Optional
+    pendo.initialize({
+        visitor: {
+            id:              'VISITOR-UNIQUE-ID2'   // Required if user is logged in, default creates anonymous ID
+            // email:        // Recommended if using Pendo Feedback, or NPS Email
+            // full_name:    // Recommended if using Pendo Feedback
+            // role:         // Optional
 
-    //         // You can add any additional visitor level key-values here,
-    //         // as long as it's not one of the above reserved names.
-    //     },
+            // You can add any additional visitor level key-values here,
+            // as long as it's not one of the above reserved names.
+        },
 
-    //     account: {
-    //         id:           'ACCOUNT-UNIQUE-ID' // Required if using Pendo Feedback, default uses the value 'ACCOUNT-UNIQUE-ID'
-    //         // name:         // Optional
-    //         // is_paying:    // Recommended if using Pendo Feedback
-    //         // monthly_value:// Recommended if using Pendo Feedback
-    //         // planLevel:    // Optional
-    //         // planPrice:    // Optional
-    //         // creationDate: // Optional
+        account: {
+            id:           'ACCOUNT-UNIQUE-ID2' // Required if using Pendo Feedback, default uses the value 'ACCOUNT-UNIQUE-ID'
+            // name:         // Optional
+            // is_paying:    // Recommended if using Pendo Feedback
+            // monthly_value:// Recommended if using Pendo Feedback
+            // planLevel:    // Optional
+            // planPrice:    // Optional
+            // creationDate: // Optional
 
-    //         // You can add any additional account level key-values here,
-    //         // as long as it's not one of the above reserved names.
-    //     }
-    // });
+            // You can add any additional account level key-values here,
+            // as long as it's not one of the above reserved names.
+        }
+    });
